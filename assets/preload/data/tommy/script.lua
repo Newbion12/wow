dc92370a84d19439d6c761036324799de9f36959
Newbion12 <96944542+Newbion12@users.noreply.github.com>
2022-07-18 20:29:16 +0900

    Add files via upload

---
+++ assets/preload/data/tommy/script.lua
@@ -0,0 +1,65 @@
+--easy script configs
+IntroTextSize = 25	--Size of the text for the Now Playing thing.
+IntroSubTextSize = 30 --size of the text for the Song Name.
+IntroTagColor = 'c46900'	--Color of the tag at the end of the box.
+IntroTagWidth = 15	--Width of the box's tag thingy.
+--easy script configs
+
+--actual script
+function onCreate()
+	--the tag at the end of the box
+	makeLuaSprite('JukeBoxTag', 'empty', -305-IntroTagWidth, 500)
+	makeGraphic('JukeBoxTag', 300+IntroTagWidth, 130, IntroTagColor)
+	setObjectCamera('JukeBoxTag', 'other')
+	addLuaSprite('JukeBoxTag', true)
+     setPropertyFromClass('ClientPrefs', 'hideHud', true);
+
+	--the box
+	makeLuaSprite('JukeBox', 'empty', -305-IntroTagWidth, 500)
+	makeGraphic('JukeBox', 300, 130, '000000')
+	setObjectCamera('JukeBox', 'other')
+	addLuaSprite('JukeBox', true)
+	
+	--the text for the "Now Playing" bit
+	makeLuaText('JukeBoxText', songName, 300, -305-IntroTagWidth, 515)
+	setTextAlignment('JukeBoxText', 'left')
+	setObjectCamera('JukeBoxText', 'other')
+	setTextSize('JukeBoxText', IntroTextSize)
+	addLuaText('JukeBoxText')
+	
+	--text for the song name
+	makeLuaText('JukeBoxSubText', 'Conehat', 300, -305-IntroTagWidth, 545)
+	setTextAlignment('JukeBoxSubText', 'left')
+	setObjectCamera('JukeBoxSubText', 'other')
+	setTextSize('JukeBoxSubText', IntroSubTextSize)
+	addLuaText('JukeBoxSubText')
+end
+
+--motion functions
+function onSongStart()
+	-- Inst and Vocals start playing, songPosition = 0
+	doTweenX('MoveInOne', 'JukeBoxTag', 0, 1, 'CircInOut')
+	doTweenX('MoveInTwo', 'JukeBox', 0, 1, 'CircInOut')
+	doTweenX('MoveInThree', 'JukeBoxText', 0, 1, 'CircInOut')
+	doTweenX('MoveInFour', 'JukeBoxSubText', 0, 1, 'CircInOut')
+	
+	runTimer('JukeBoxWait', 3, 1)
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	-- A loop from a timer you called has been completed, value "tag" is it's tag
+	-- loops = how many loops it will have done when it ends completely
+	-- loopsLeft = how many are remaining
+	if tag == 'JukeBoxWait' then
+		doTweenX('MoveOutOne', 'JukeBoxTag', -450, 1.5, 'CircInOut')
+		doTweenX('MoveOutTwo', 'JukeBox', -450, 1.5, 'CircInOut')
+		doTweenX('MoveOutThree', 'JukeBoxText', -450, 1.5, 'CircInOut')
+		doTweenX('MoveOutFour', 'JukeBoxSubText', -450, 1.5, 'CircInOut')
+	end
+end
+function onStartCountdown()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'good-gay');
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx-chuck');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver-chuck');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd-chuck');
+end
